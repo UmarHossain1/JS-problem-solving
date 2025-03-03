5bb7946e4969457c31734b87224c5cfba580f89b
umarhossain1 <umarhossain2000@gmail.com>
2025-03-03 22:50:04 +0600

    second problem solve

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// *problem 1
+
 function calculateTax(income, expenses) {
   if (income <= 0 || expenses <= 0 || expenses > income) {
     return "Invalid Input";
@@ -8,4 +10,20 @@ function calculateTax(income, expenses) {
   const tax = difference * 0.2;
 
   return tax;
+}
+
+// *problem 2
+
+function sendNotification(email) {
+  if (email.indexOf("@") === -1) {
+    return "Invalid Email";
+  }
+
+  const check = email.split("@");
+
+  const userName = check[0];
+
+  const domainName = check[1];
+
+  return userName + " sent you an email from " + domainName;
 }
